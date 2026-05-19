0c68a433b5fe14e14463ad0c860ca640410269c4
Patocalipto <patricio.pantoja.lee@gmail.com>
2026-05-19 10:17:38 -0600

    fix: correct Vite and GitHub Pages configuration

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Reemplaza 'My-Portfolio' por el nombre exacto de tu repositorio en GitHub
+  base: '/My-Portfolio/',
+})
